3fe888e5400c1480a3433fbdb9f13c6e40585128
Sam Suh <suhvivor24@gmail.com>
2022-10-14 16:55:14 -0700

    part of tags

---
+++ app/forms/tags_form.py
@@ -0,0 +1,47 @@
+from xmlrpc.client import Boolean
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, SubmitField
+
+
+class TagForm(FlaskForm):
+    acai_bowls_tag = BooleanField('Acai Bowls')
+    bagels_tag = BooleanField('Bagels')
+    bakeries_tag = BooleanField('Bakeries')
+    beer_wine_spirits_tag = BooleanField('Beer, Wine & Spirits')
+    breweries_tag = BooleanField('Breweries')
+    bubble_tea_tag = BooleanField('Bubble Tea')
+    butcher_tag = BooleanField('Butcher')
+    coffee_tea_tag = BooleanField('Coffee & Tea')
+    convenience_store_tag = BooleanField('Convenience Stores')
+    delicatessen_tag = BooleanField('Delicatessen')
+    desserts_tag = BooleanField('Desserts')
+    donuts_tag = BooleanField('Donuts')
+    farmers_market_tag = BooleanField('Famers Market')
+    food_deliever_services_tag = BooleanField('Food Delivery Services')
+    food_trucks_tag = BooleanField('Food Trucks')
+    gelato_tag = BooleanField('Gelato')
+    grocery_tag = BooleanField('Grocery')
+    honey_tag = BooleanField('Honey')
+    ice_cream_frozen_yogurt_tag = BooleanField('Ice Cream & Frozen Yogurt')
+    internet_cafes_tag = BooleanField('Internet Cafes')
+    juice_bars_smoothies_tag = BooleanField('Juice Bars & Smoothies')
+    poke_tag = BooleanField('Poke')
+    shaved_ice_tag = BooleanField('Shaved Ice')
+    tortillas_tag = BooleanField('Tortillas')
+    afghan_tag = BooleanField('Afghan')
+    african_tag = BooleanField('African')
+    american_tag = BooleanField('American')
+    asian_fusion_tag = BooleanField('Asian Fusion')
+    barbeque_tag = BooleanField('Barbeque')
+    bistros_tag = BooleanField('Bistros')
+    brazillian_tag = BooleanField('Brazillian')
+    breakfast_brunch_tag = BooleanField('Breakfast & Brunch')
+
+
+
+
+
+
+
+
+
