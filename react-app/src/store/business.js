fa6cf638ad592f43e3428c2f279f2c6dee9906ec
jake matillano <jakematillano.z@gmail.com>
2022-10-15 18:59:45 -0700

    thunk action creators get single business checkpoint for business store

--- react-app/src/store/business.js
+++ react-app/src/store/business.js
@@ -1,60 +1,80 @@
 
 // Action Types
-const LOAD_ALL = "businesses/LOAD_ALL"
-const LOAD_CURRENT = "businesses/LOAD_CURRENT"
-const LOAD_ONE = "businesses/LOAD_ONE"
-const CREATE = "businesses/CREATE"
-const UPDATE = "businesses/UPDATE"
-const REMOVE = "businesses/DELETE"
+const LOAD_ALL = "businesses/LOAD_ALL";
+const LOAD_CURRENT = "businesses/LOAD_CURRENT";
+const LOAD_ONE = "businesses/LOAD_ONE";
+const CREATE = "businesses/CREATE";
+const UPDATE = "businesses/UPDATE";
+const REMOVE = "businesses/DELETE";
 
 //Action Creators
 const loadAll = (businesses) => ({
   type: LOAD_ALL,
   businesses
-})
+});
 
 const loadCurrent = (businesses) => ({
   type: LOAD_CURRENT,
   businesses
-})
+});
 
 const loadOne = (business) => ({
   type: LOAD_ONE,
   business
-})
+});
 
 const create = (business) => ({
   type: CREATE,
   business
-})
+});
 
 const update = (business) => ({
   type: UPDATE,
   business
-})
+});
 
 const remove = businessId => ({
   type: REMOVE,
   businessId
-})
+});
 
 // THUNK action creators
-
 export const getAllBusinesses = () => async (dispatch) => {
-  const response = await fetch("/api/businesses")
-  const businessData = await response.json()
+  const response = await fetch("/api/businesses");
+  const businessData = await response.json();
 
   if (response.ok) {
-    await dispatch(loadAll(businessData))
+    dispatch(loadAll(businessData));
   }
-}
+  else {
+    return console.log("Get All Business Thunk Error");
+  }
+  return businessData;
+};
 
 export const getCurrentUserBusinesses = () => async (dispatch) => {
-  const response = await fetch("/api/businesses/current")
-  const businessData = await response.json()
+  const response = await fetch("/api/businesses/current");
+  const businessData = await response.json();
 
-  if(response.ok) {
-    await dispatch(loadCurrent(businessData))
+  if (response.ok) {
+    dispatch(loadCurrent(businessData));
   }
-  return businessData
+  else {
+    return console.log("Get Current User Businesses Thunk Error")
+  }
+  return businessData;
+};
+
+export const getSingleBusiness = (business) => async (dispatch) => {
+  const response = await fetch(`/api/businesses/${business.id}`);
+  const singleBusinessData = await response.json();
+
+  if (response.ok) {
+    dispatch(loadOne(singleBusinessData))
+  }
+  else {
+    console.log("Get Single Business Thunk Error")
+  }
+  return singleBusinessData
+
 }
