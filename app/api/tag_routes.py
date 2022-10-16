b6c24c4444d9b695f3efb72ccfad5df21452e593
garydsong <ceouls@gmail.com>
2022-10-16 15:08:37 -0700

    fixed tag route and added blueprint and to_dict function to tag model

--- app/api/tag_routes.py
+++ app/api/tag_routes.py
@@ -5,8 +5,13 @@ from app.models import Tag
 tag_routes = Blueprint('tags', __name__)
 
 
-@tag_routes.route('/')
+@tag_routes.route('/', methods=["GET"])
 def get_all_tags():
     tags = Tag.query.all()
+    tag_list = []
 
-    return jsonify(tags)
+    for tag in tags:
+        tags_dict = tag.to_dict()
+        tag_list.append(tags_dict)
+
+    return {"tags": [tag for tag in tag_list]}
