32ca14ec323310219faedd33d8e923ee42556c43
alex dam <alexdam55@gmail.com>
2022-10-17 09:29:22 -0700

    current user review functionality implemented, need to test for bugs

--- react-app/src/components/Reviews/CurrentUserReviews/index.js
+++ react-app/src/components/Reviews/CurrentUserReviews/index.js
@@ -11,10 +11,31 @@ const CurrentUserReviews = () => {
     const reviews = useSelector(state => state.reviews)
 
     useEffect(() => {
-        dispatch(getCurrent())
+        dispatch(getCurrentReviews())
         return () => dispatch(reset())
     }, [dispatch])
-    return ()
+    if (reviews === undefined || !Object.values(reviews).length) {
+        return (
+            <div>Looks like you dont have any Reviews!</div>
+        )
+    } else {
+        return (
+            <div>
+                {reviews && Object.values(reviews).length &&
+                <div>
+                    <h2>Person's Reviews!</h2>
+                    <div>
+                        {Object.values(reviews).map(review => {
+                            <div key={review.id}>
+                                {review.review}
+                            </div>
+                        })}
+                    </div>
+                </div>
+                }
+            </div>
+        )
+    }
 }
 
 export default CurrentUserReviews
