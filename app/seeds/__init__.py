263e4ebae87a2ae3aeff31fb78846f8fede82fb7
alex dam <alexdam55@gmail.com>
2022-10-17 11:01:36 -0700

    business_tags seed data

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -4,6 +4,7 @@ from .businesses import seed_businesses, undo_businesses
 from .reviews import seed_reviews, undo_reviews
 from .business_images import seed_business_images, undo_business_images
 from .tag import seed_tags, undo_tags
+from .business_tags import seed_bussiness_tags, undo_business_tags
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -18,6 +19,7 @@ def seed():
     seed_reviews()
     seed_business_images()
     seed_tags()
+    seed_bussiness_tags()
     # Add other seed functions here
 
 
@@ -29,4 +31,5 @@ def undo():
     undo_reviews()
     undo_business_images()
     undo_tags()
+    undo_business_tags()
     # Add other undo functions here
