7ff6c0b38ca7b295db2454a70f95fff94cb727bb
alex dam <alexdam55@gmail.com>
2022-10-16 21:18:45 -0700

    business details component in progress

--- react-app/src/components/Reviews/BusinessReviews/BusinessReviews.js
+++ react-app/src/components/Reviews/BusinessReviews/BusinessReviews.js
@@ -1,1 +1,19 @@
 //  empty for now TODO
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory, useParams } from 'react-router-dom'
+import { getAllReviews } from '../../../store/review'
+import './BusinessReviewPage.css'
+
+const BusinessReview = () => {
+    const { businessId } = useParams()
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const user = useSelector(state => state.session.user)
+    const businessReviews = useSelector(state => state.reviews)
+
+    useEffect(() => {
+        dispatch(getAllReviews(businessId))
+    }, [dispatch])
+}
