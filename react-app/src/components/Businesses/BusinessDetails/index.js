03d6b743b7683283aff48428c66750aa1da3920d
Sam Suh <suhvivor24@gmail.com>
2022-10-16 20:15:30 -0700

    business-details basic structure rendering, un-styled

--- react-app/src/components/Businesses/BusinessDetails/index.js
+++ react-app/src/components/Businesses/BusinessDetails/index.js
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getSingleBusinessThunk } from '../../../store/business';
-import { useParams, useHistory} from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
+import './BusinessDetails.css'
 
 const BusinessDetails = () => {
     const dispatch = useDispatch();
-    const businesses = useSelector(state => state.businesses);
+    const history = useHistory();
     const params = useParams();
     const { businessId } = params;
-    const history = useHistory();
+    const business = useSelector(state => state.businesses[businessId]);
     const [isLoaded, setIsLoaded] = useState(false)
 
     useEffect(() => {
@@ -16,15 +17,51 @@ const BusinessDetails = () => {
         // console.log('what the fuck', pause)
         console.log('Business Id', businessId)
         dispatch(getSingleBusinessThunk(businessId))
-        .then(() => {setIsLoaded(true)})
-        
-    }, [dispatch, businessId ])
+            .then(() => { setIsLoaded(true) })
+
+    }, [dispatch, businessId])
 
 
     return isLoaded && (
-        
-        <div>
-            <h1> HELLO {businesses[businessId].business_name} </h1>
+        <div id='business-details-page'>
+            <div id='business-details-header-images'>
+                <div id='business-details-header-content'>
+                    <div id='business-details-header-info-container'>
+                        <div id='business-details-logo'>
+                            <span>BUSINESS LOGO IMG</span>
+                        </div>
+                        <div id='business-details-info'>
+                            <div>{business.business_name}</div>
+                        </div>
+                    </div>
+                    <button id='all-photos-button'>All Photos</button>
+                </div>
+            </div>
+            <div id='business-details-container'>
+                <div id='details-content'>
+                    <div id='business-details-action-buttons'>
+                        <button>Write a Review</button>
+                        <button>Add a photo </button>
+                    </div>
+                    <section id='business-details-amenities'></section>
+                    <section id='business-details-about-container'>
+                        <div> <h1>About the Business</h1> </div>
+                        <div id='about-content'>
+                            <div id='business-details-owner-avatar'></div>
+                            <div id='owner-name-title-div-column'>
+                                <div id='business-details-owner-name'></div>
+                                <div id='business-details-owner-title'></div>
+                            </div>
+                        </div>
+                        <div> {business.about}</div>
+                    </section>
+                </div>
+                <div id='sticky-sidebar-container'>
+                    <div id='sticky-sidebar-content'>
+                        {business.website}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
