3c3108dfae5b8d4009b29333bc9f55de40408ce2
jake matillano <jakematillano.z@gmail.com>
2022-10-17 15:11:11 -0700

    card component added

--- react-app/src/components/Businesses/BusinessCard/BusinessCard.js
+++ react-app/src/components/Businesses/BusinessCard/BusinessCard.js
@@ -33,6 +33,31 @@ export default function BusinessCard(){
         </div>
       </div>
       {/* MIDDLE DIV START */}
+      <div id="center-div-container">
+
+        <h3 style={{ paddingLeft: "20px" }}>All "search" Results in "City","State"</h3>
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%" }}>
+
+          <Link id='business-card-link'>
+          <div style={{ display: "flex", width: "800px", height: "275px", justifyContent: "flex-start", alignItems: "center", borderBottom: "1px solid lightgrey", padding: "0px 20px"}}>
+            <div>
+              <img style={{ backgroundColor: "blue", width: "200px", height: "200px", borderRadius: "8px" }}></img>
+            </div>
+            <div style={{ paddingLeft: "20px", display: "flex", flexDirection: "column", marginTop: "-65px" }}>
+              <div id="business-card-business-name">Business Name</div>
+              <div>Grumbles go here | avg rating (review count)</div>
+              <div><button className="tag-button">Tag1</button>{" "}<button className="tag-button">Tag2</button>{" "}<button className="tag-button">Tag3</button>{" "}<span>$$ &#x2022;
+              </span>{" "}<span>City</span></div>
+              <br></br>
+              <div>Some randoom comment here blah blah  blah blah blah blah blah blah blah blah blahblahblahblah blah  vv blah blah blah blah{" "}<Link id="more-link">more</Link></div>
+            </div>
+          </div>
+          </Link>
+
+        </div>
+
+      </div>
+      {/* MIDDLE DIV END */}
     </div>
   )
 }
