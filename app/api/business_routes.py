e4d90e8aa5861593327d61df0b47c114739ed975
jake matillano <jakematillano.z@gmail.com>
2022-10-18 16:04:41 -0700

    test

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -60,12 +60,8 @@ def get_business_by_id(id):
   business_dict['Reviews'] = [review.to_dict() for review in reviews]
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> jake-main
-  # businessImages = BusinessImage.query.filter(BusinessImage.business_id == id)
-  # business_dict['BusinessImages'] = [businessImage.to_dict() for businessImage in businessImages]
+  businessImages = BusinessImage.query.filter(BusinessImage.business_id == id)
+  business_dict['BusinessImages'] = [businessImage.to_dict() for businessImage in businessImages]
   businessImages = business.business_images
   business_dict["businessImages"] = [businessImage.to_dict() for businessImage in businessImages]
 
