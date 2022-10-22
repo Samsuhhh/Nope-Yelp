8424bc0c931231a86166042e75fe97d1de7777e3
garydsong <ceouls@gmail.com>
2022-10-21 20:40:29 -0700

    logo link in for businesses

--- react-app/src/components/Businesses/AddBusinessImage/index.js
+++ react-app/src/components/Businesses/AddBusinessImage/index.js
@@ -53,7 +53,9 @@ export default function AddBusinessImage() {
         <>
             <div className="add-business-nav-bar">
                 <div className="add-business-nav-bar-content-wrapper">
+                <Link to={`/businesses/${businessId}`}>
                     <img id="add-business-nav-bar-logo" src={nope} />
+                    </Link>
                     <Link to={`/businesses/${businessId}`}>
                         <div className="add-business-nav-bar-back-to-nope">Back to your page</div></Link>
                 </div>
