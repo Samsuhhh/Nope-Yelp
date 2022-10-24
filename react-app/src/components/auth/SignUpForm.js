8cd2cd405502644cb5e6d688760828de4bbacc87
Sam Suh <suhvivor24@gmail.com>
2022-10-23 20:14:26 -0700

    signup error

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -4,9 +4,10 @@ import { Redirect, useHistory } from 'react-router-dom';
 import { signUp } from '../../store/session';
 import Footer from '../Footer/Footer';
 import nopewhite from '../../assets/nope-white.png'
-import unhappy from '../../assets/imgs/unhappy.png'
-import speechbox from '../../assets/imgs/speechbox.svg'
 import './SignUpForm.css'
+import signupgood from '../../assets/imgs/sign-up-good.png';
+import blacknope from '../../assets/nopes/ratingimgblack.png';
+
 
 const SignUpForm = () => {
   const user = useSelector(state => state.session.user);
@@ -42,11 +43,12 @@ const SignUpForm = () => {
     if (firstName.length < 2 || firstName.length > 15) vErrors.push('First name must be between 2 and 15 characters.')
     if (lastName.length < 2) vErrors.push('Last name must be greater than 2 characters.')
     if (username.length < 5 || username.length > 15) vErrors.push('Username must be between 5 and 15 characters.')
+    if (!email.match(/^\S+@\S+\.\S+$/)) vErrors.push('Please enter a valid email address')
     if (password.length < 6 || password.length > 15) vErrors.push('Password must be between 5 and 15 characters.')
     if (password !== confirmPassword) vErrors.push('Password fields do not match!')
 
     setErrors(vErrors)
-  }, [firstName, lastName, username, password, confirmPassword])
+  }, [firstName, lastName, username, password, confirmPassword, email])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -71,6 +73,10 @@ const SignUpForm = () => {
   const handleCancel = async (e) => {
     e.preventDefault();
     history.push('/')
+  }
+
+  if (user) {
+    return <Redirect to='/' />;
   }
 
 
@@ -120,7 +126,7 @@ const SignUpForm = () => {
   return (
     <>
       <div className='sign-up-header'>
-        <a href="/"><img id="signup-logo" src={nopewhite} /></a>
+        <a href="/"><img alt='white nope' id="signup-logo" src={nopewhite} /></a>
       </div>
 
       {/* <div id="error-box">
@@ -144,16 +150,15 @@ const SignUpForm = () => {
         </div> */}
 
       <div id="sign-up-form-wrapper">
+        <div id='signUp-header'>
+          Become a Noper!
+        </div>
 
         <div id="signUp-form-left">
-          <div id='signUp-header'>
-            Sign Up
-          </div>
-
           <div id='signUp-form-content'>
             <form className="sign-up-form" onSubmit={handleSubmit}>
-              <div id='fragmented-first-last-container'>
-                <div className='signup-input-divs'>
+              <div className='fragmented-divs-container-address-LL-url'>
+                <div className='fragmented-div-styling'>
                   <input
                     id="first-name-input"
                     type='text'
@@ -163,7 +168,7 @@ const SignUpForm = () => {
                     value={firstName}
                   ></input>
                 </div>
-                <div className='signup-input-divs'>
+                <div className='fragmented-div-styling'>
                   <input
                     id="last-name-input"
                     type='text'
@@ -174,10 +179,9 @@ const SignUpForm = () => {
                   ></input>
                 </div>
               </div>
-              <div className='signup-input-divs-single'>
+              <div className='create-input-divs'>
                 <input
-                  className='signup-input'
-                  id="username-input"
+                  className='create-business-input'
                   type='text'
                   name='username'
                   placeholder='Username'
@@ -185,21 +189,19 @@ const SignUpForm = () => {
                   value={username}
                 ></input>
               </div>
-              <div className='signup-input-divs-single'>
+              <div className='create-input-divs'>
                 <input
-                  className='signUp-input'
-                  id="email-input"
-                  type='email'
+                  className='signup-input'
+                  type='text'
                   name='email'
                   placeholder='Email'
                   onChange={updateEmail}
                   value={email}
                 ></input>
               </div>
-              <div className='signup-input-divs-single'>
+              <div className='create-input-divs'>
                 <input
-                  className='signUp-input'
-                  id="password-input"
+                  className='signup-input'
                   type='password'
                   name='password'
                   placeholder='Password'
@@ -207,32 +209,50 @@ const SignUpForm = () => {
                   value={password}
                 ></input>
               </div>
-              <div className='signup-input-divs-single'>
+              <div className='create-input-divs'>
                 <input
-                  className='signUp-input'
-                  id="password-input"
+                  className='signup-input'
                   type='password'
-                  name='repeat_password'
+                  name='confirm_password'
                   placeholder='Confirm Password'
                   onChange={updateConfirmPassword}
                   value={confirmPassword}
                   required={true}
                 ></input>
               </div>
-              <button id="signup-submit-button" type='submit'>Sign Up</button>
-              <button id='signup-cancel-button' onClick={handleCancel}>Cancel</button>
+              <div id='action-buttons'>
+                <button id="signup-submit-btn" type='submit'>Sign Up</button>
+                <button id='signup-cancel-btn' onClick={handleCancel}>Cancel</button>
+              </div>
             </form>
           </div>
+          <div id='signup-goop-container'>
+            <img alt='mini goop' id="mini-goop-img" src={signupgood} />
+            {showErrors &&
+              <div id={errors.length ? 'signup-validations-div' : 'hidden'}>
+                {errors.map((e, i) => {
+                  return (
+                    <div id='error-div' key={i}>
+                      <div>
+                        <img alt='black nope' id='bNope' src={blacknope} />
+                      </div>
+                      {e}
+                    </div>
+                  )
 
-        </div>
-        <div id='signUp-form-right-side'>
-          <div id='goop-container'>
-            <img id="unhappy-img" src={unhappy} />
+                })}
+
+              </div>
+            }
+
 
           </div>
-
-
         </div>
+        {/* <div id='signUp-form-right-side'>
+
+
+
+        </div> */}
       </div>
       <Footer />
     </>
